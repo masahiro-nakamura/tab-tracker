bfd316e54e3e5409a00de1d7c3d6dd435b975dce
masahiro-nakamura <masahiro.nakamura.1992@gmail.com>
2017-12-06 06:50:59 +0900

    add register methods

---
+++ client/src/components/Register.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email" />
+    <br>
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password" />
+    <br>
+    <button
+      @click="register">
+      Register
+    </button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  // watch: {
+  //   email (value) {
+  //     console.log('email has changed', value)
+  //   }
+  // },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+      // console.log('register button was pushed.', this.email, this.password)
+    }
+  }
+  // mounted (){
+  //   setTimeout(() => {
+  //     this.email = 'Hello world'
+  //   }, 1000)
+  // }
+}
+</script>
+
+<style scoped>
+
+</style>
